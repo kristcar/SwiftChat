72f0e56f14150e6b7eb7276272b3354fb0c492bd
Kristin <kristincaras1@gmail.com>
2022-07-21 08:34:02 -0400

    code added in ChatViewController for scroll functionality

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -56,6 +56,10 @@ class ChatViewController: UIViewController {
                             
                             DispatchQueue.main.async {
                                 self.tableView.reloadData()
+                                
+                                //scroll functionality:
+                                let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                                self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
                             }
                             
                         }
